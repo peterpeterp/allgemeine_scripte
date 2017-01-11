fad36aede5b6cc02776f1f274e42aa39496da4a7
Peter <peterpfleiderer@gmx.de>
2017-01-11 11:10:40 +0100

    fixed cep plotting issue

--- plot_functions.py
+++ plot_functions.py
@@ -27,7 +27,18 @@ def make_colormap(seq):
 col_conv = mcolors.ColorConverter().to_rgb
 
 # simpler method
-def plot_map(ax,lon,lat,Z,color_type=plt.cm.bwr,color_range=[0,100],color_label='bla',subtitle='',grey_area=None):
+def plot_map(ax,lon,lat,Z,color_type=plt.cm.bwr,color_range=[0,100],color_label='',subtitle='',grey_area=None):
+
+	# handle 0 to 360 lon
+	if max(lon)>180:
+		problem_start=np.where(lon>180)[0][0]
+		new_order=np.array(range(problem_start,len(lon))+range(0,problem_start))
+		Z=Z[:,new_order]
+		lon=lon[new_order]
+		lon[lon>180]-=360
+
+	# get limits
+	print np.min(lon),np.max(lon),np.min(lat),np.max(lat)
 	m = Basemap(ax=ax,llcrnrlon=np.min(lon),urcrnrlon=np.max(lon),llcrnrlat=np.min(lat),urcrnrlat=np.max(lat),resolution="l",projection='cyl')
 	m.drawmapboundary(fill_color='1.')
 
@@ -61,7 +72,57 @@ def plot_map(ax,lon,lat,Z,color_type=plt.cm.bwr,color_range=[0,100],color_label=
 	return(ax,im1)
 
 # # could be better for specific grid comparisons
-# def plot_map(ax,lon,lat,Z,color_type=plt.cm.bwr,color_range=[0,100],color_label='bla',subtitle='',grey_area=None,limits=None):
+# def plot_map_colormesh(ax,lon,lat,Z,color_type=plt.cm.bwr,color_range=[0,100],color_label='bla',subtitle='',grey_area=None,limits=None):
+# 	if limits==None:
+# 		limits=[np.min(lon),np.max(lon),np.min(lat),np.max(lat)]
+# 		print limits
+		
+# 	m = Basemap(ax=ax,llcrnrlon=limits[0],urcrnrlon=limits[1],llcrnrlat=limits[2],urcrnrlat=limits[3],resolution="l",projection='cyl')
+# 	m.drawmapboundary(fill_color='1.')
+
+# 	Zm=np.ma.masked_invalid(Z)
+
+# 	# create lon lat mesh 
+# 	lons=lon.copy()
+# 	step=np.diff(lons,1)[0]
+# 	lons-=step/2
+# 	lons=np.append(lons,np.array(lons[-1]+step))
+
+# 	lats=lat.copy()
+# 	step=np.diff(lats,1)[0]
+# 	lats-=step/2
+# 	lats=np.append(lats,lats[-1]+step)
+
+# 	lons,lats = np.meshgrid(lons,lats)
+
+# 	print lons,lats
+
+
+# 	im1 = m.pcolormesh(lons,lats,Zm,cmap=color_type,vmin=color_range[0],vmax=color_range[1])
+# 	if grey_area!=None:
+# 		grey_area=np.ma.masked_invalid(grey_area)
+# 		im2 = m.pcolormesh(lons,lats,grey_area,cmap=plt.cm.Greys,vmin=0,vmax=1)
+
+# 	m.drawcoastlines()
+# 	m.drawstates()
+# 	m.drawcountries()
+
+	
+# 	if color_label!=None:
+# 		# add colorbar
+# 		cb = m.colorbar(im1,'right', size="5%", pad="2%")
+
+# 		tick_locator = ticker.MaxNLocator(nbins=5)
+# 		cb.locator = tick_locator
+# 		cb.update_ticks()
+# 		cb.set_label(color_label, rotation=90)
+
+# 	if subtitle!='':ax.set_title(subtitle)
+
+# 	return(ax,im1)
+
+# # could be better for specific grid comparisons
+# def plot_map_old(ax,lon,lat,Z,color_type=plt.cm.bwr,color_range=[0,100],color_label='bla',subtitle='',grey_area=None,limits=None):
 # 	if limits==None:
 # 		limits=[np.min(lon,axis=1)[0]-1,np.max(lon,axis=1)[0]+1,np.min(lat,axis=0)[0]-1,np.max(lat,axis=0)[0]+1]
 # 		print limits
